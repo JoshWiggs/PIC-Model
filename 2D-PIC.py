31a58cef49c2180169b6cd2640851998071e9749
Josh Wiggs <josh.wiggs@outlook.com>
2019-03-13 14:18:14 +0000

    Vectorized parts for improved performance and added particle regen opt

--- 2D-PIC.py
+++ 2D-PIC.py
@@ -1,6 +1,7 @@
 import numpy as np
 import random as ran
 import scipy.constants as con
+import scipy.stats as stats
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 
@@ -8,15 +9,40 @@ import matplotlib.cm as cm
 class options:
 
     def __init__(self):
-            self.DebyeTest = bool(False)
+            self.DebyeTest = bool(True)
+            self.RegenerateParticles = bool(False)
 
     class visual:
 
         def __init__(self):
-            self.write_movie = bool(True)
+            self.write_movie = bool(False)
 
 opt = options()
 vis = options.visual()
+
+class maxwell_velocities(stats.rv_continuous):
+    def _pdf(self,v):
+        return (((con.m_e)/(2*con.pi*con.k*300))**(3/2))*(4*con.pi*(v**2))*np.exp(-((con.m_e*(v)**2)/(2*con.k*300)))
+
+velocity_pdf = maxwell_velocities(name='velocity_pdf', a=0.0, b=con.c)
+
+def thermal_velocity(T,Par_m):
+
+    #Open variables
+    varience,u1,u2,v = float(0)
+
+    #Calculate varience in eV
+    varience = T*1.602e-19/Par_m
+
+    #Generate random uniform numbers between 0 & 1
+    u1 = ran.uniform(0.0,1.0)
+    u2 = ran.uniform(0.0,1.0)
+
+    #use Box-Muller transform to generate randomly-distributed normal
+    v = np.sqrt(-2.0*np.log(u1))*np.cos(2.0*np.pi*u2)*np.sqrt(variance)
+
+    v_x = v*u1
+    v_y = v*u2
 
 if vis.write_movie is True:
 
@@ -28,21 +54,21 @@ if vis.write_movie is True:
 
 #Define input parameters
 x_min = 0
-x_max = 1
+x_max = 10
 y_min = 0
-y_max = 1
+y_max = 10
 t_min = 0
 t_max = 2.0
-v_min = -0.10
-v_max = 0.10
-q_c = [1,-1] #particle charge
-m = [938000000,511000] #particle mass
+v_min = -1
+v_max = 1
+q_c = [con.e,-(10*con.e)] #particle charge
+m = [con.m_p,(10*con.m_e)] #particle mass
 
-nx = 20 #Number of steps taken from y_min to y_max
-ny = 20 #Number of steps taken from x_min to x_max
-nt = 1000 #Number of time steps
-n_smoothing = 200 #For differencing methods that require smoothing
-PPC = 1 #Number of particles per cell
+nx = 50 #Number of steps taken from y_min to y_max
+ny = 50 #Number of steps taken from x_min to x_max
+nt = 200 #Number of time steps
+n_smoothing = 100 #For differencing methods that require smoothing
+PPC = 0.2 #Number of particles per cell
 
 phi = np.zeros(((nx + 1),(ny + 1))) #Electric potential
 E_x = np.zeros(((nx + 1),(ny + 1))) #x component of the Electric Field
@@ -53,7 +79,7 @@ dx = (abs(x_min) + x_max) / nx #Size of step in x domain
 dy = (abs(y_min) + y_max) / ny #Size of step in y domain
 dt = (abs(t_min) + t_max) / nt #Size of step on the temporal domain
 T_c = nx * ny #Total number of cells
-T_PPC = T_c * PPC #Total number of particles in simulation
+T_PPC = int(T_c * PPC) #Total number of particles in simulation
 
 #Populate dimesnional vectors
 x = np.linspace(x_min,x_max,num=nx)
@@ -83,9 +109,12 @@ if vis.write_movie is True:
     w,h = fig.canvas.get_renderer().get_canvas_width_height()
 
     # Setup the imageio writer.
-    writer = imageio.get_writer('electrons_protons.gif', fps=50)
+    writer = imageio.get_writer('regen_electron_proton.gif', fps=25)
+
+print('Initialising {} particles...'.format(T_PPC))
 
 for i in range(0,T_PPC):
+
     #TODO: Generalise
     j = ran.randint(0,int(len(q_c)-1)) #Random integer for mass and charge selection
 
@@ -93,6 +122,8 @@ for i in range(0,T_PPC):
     Par_y[i] = ran.uniform(y_min,y_max) #Particle y coordinate
     Par_vx[i] = ran.uniform(v_min,v_max) #Particle velocity on the x direction
     Par_vy[i] = ran.uniform(v_min,v_max) #Particle velocity on the y direction
+    #Par_vx[i] = (velocity_pdf.rvs() * 0.001)  #Particle velocity on the x direction
+    #Par_vy[i] = (velocity_pdf.rvs() * 0.001) #Particle velocity on the y direction
     Par_q[i] = q_c[j] #Particle charge
     Par_m[i] = m[j] #Particle mass
 
@@ -100,24 +131,29 @@ for i in range(0,T_PPC):
 #TODO: Rework this as data types produced seem to make calcultions difficult
 Par = list(zip(Par_x,Par_y,Par_vx,Par_vy,Par_q,Par_m))
 
-##### If the Debye test is active then generate the Debye test particle and
-##### append it as the final item in the particle list
+#If the Debye test is active then generate the Debye test particle and
+#append it as the final item in the particle list
 if opt.DebyeTest is True:
+
+    print('Creating Debye particle...')
 
     D_Par_x = ((x_min + x_max) / 2)
     D_Par_y = ((y_min + y_max) / 2)
     D_Par_vx = 0.0
     D_Par_vy = 0.0
-    D_Par_q = 100000.0
-    D_Par_m = 938000000
+    D_Par_q = (con.e * 10000)
+    D_Par_m = (con.m_p * 1)
 
     D_Par = [D_Par_x,D_Par_y,D_Par_vx,D_Par_vy,D_Par_q,D_Par_m]
 
     Par.append(D_Par)
 
+print('Initialising particle simulation...')
+
 #Numerical loop responsible for interating the particles through time
 for t in range(0,nt):
 
+    #Activate condition only if writing gif
     if vis.write_movie is True:
 
         #Clear the axes so we can draw the new frame
@@ -136,6 +172,8 @@ for t in range(0,nt):
     #is done by calculating grid positions and deleting particles that lie on
     #a grid ID outside the area.
     while i < T_PPC:
+
+        #Open / clear variables for use
         x_i = 0
         y_i = 0
 
@@ -175,6 +213,35 @@ for t in range(0,nt):
 
         #Progress to next particle
         i += 1
+
+    #If option to regenerate particles that leave the simulation domain is
+    #active then include this step in iteration loop
+    if opt.RegenerateParticles is True:
+
+        #Calculate number of particles that require regenerating
+        Regenerate_num = int((T_c * PPC) - len(Par))
+
+        #If particles require regenerating then active loop to generate insertion
+        #parameters for as many new particles as required
+        if Regenerate_num > 0:
+            for r in range(0,Regenerate_num):
+
+                #This works in the same way as the loop in the pre-process
+                j = ran.randint(0,int(len(q_c)-1))
+
+                Regen_par_x = ran.uniform(x_min,x_max)
+                Regen_par_y = ran.uniform(y_min,y_max)
+                Regen_par_vx = ran.uniform(v_min,v_max)
+                Regen_par_vy = ran.uniform(v_min,v_max)
+                Regen_par_q = q_c[j]
+                Regen_par_m = m[j]
+
+                Regen_par = [Regen_par_x,Regen_par_y,Regen_par_vx,Regen_par_vy,Regen_par_q,Regen_par_m]
+
+                #Insert regenerated particles into particle list preserving the
+                #position of the current last element incase Debye test is
+                #active
+                Par.insert(int(len(Par)-2),Regen_par)
 
     #DEBUG: Inform that all particles have been checked to ensure they are in
     #the simulation area
@@ -227,14 +294,23 @@ for t in range(0,nt):
 
     #Step 2: Compute Electric Potential
     for n in range(0,n_smoothing):
+
+        """
         for i in range(1,nx):
             for j in range(1,ny):
 
-                phi[i][j] = (q_grid[i][j]*((dx**2)*(dy**2)) + ((dy**2)*(phi[i-1][j]
-                +phi[i+1][j])) + ((dx**2)*(phi[i][j-1]+phi[i][j+1])) / (2*((dy**2)
-                +(dy**2))))
+                phi[i][j] = (q_grid[i][j]*((dx**2)*(dy**2)) + ((dy**2)*
+                (phi[i-1][j] +phi[i+1][j])) + ((dx**2)*(phi[i][j-1]+phi[i][j+1]))
+                 / (2*((dy**2) +(dy**2))))
+        """
+
+        phi = (q_grid*((dx**2)*(dy**2)) + ((dy**2)*(np.roll(phi,1,axis=0)
+        + np.roll(phi,-1,axis=0))) + ((dx**2)*(np.roll(phi,1,axis=1)
+        + np.roll(phi,-1,axis=1))) / (2*((dy**2) +(dy**2))))
 
     #Step 3: Compute Electric Field
+
+    """
     for i in range(1,nx):
         for j in range(1,ny):
 
@@ -243,6 +319,16 @@ for t in range(0,nt):
             E_y[i][j] = ((phi[i][j + 1]) - phi[i][j - 1]) / (2 * dy)
 
             E_mag[i][j] = np.sqrt((E_x[i][j]**2) + (E_y[i][j]**2))
+    """
+
+    E_x = ((np.roll(phi,-1,axis=0)) - np.roll(phi,1,axis=0)) / (2 * dx)
+
+    E_y = ((np.roll(phi,-1,axis=1)) - np.roll(phi,1,axis=1)) / (2 * dy)
+
+    E_mag = np.sqrt((E_x**2) + (E_y**2))
+    E_mag = np.delete(E_mag,-1,axis=0)
+    E_mag = np.delete(E_mag,-1,axis=1)
+
 
     #Step 4: Move particles
     for i in range(0,T_PPC):
@@ -305,10 +391,11 @@ for t in range(0,nt):
             plt.subplot(2,1,1)
             plt.contourf(X,Y,E_mag)
             plt.colorbar(label = '$|E|$')
-            plt.quiver(X,Y,Par_vx_plot,Par_vy_plot,color='white')
+            plt.title('$t$ = {}, Particle Number = {}'.format(round(t*dt,2),T_PPC))
+            #plt.quiver(X,Y,Par_vx_plot,Par_vy_plot,color='white')
             plt.subplot(2,1,2)
-            plt.scatter(Par_x_plot,Par_y_plot)
-            #plt.colorbar(label = '$q$')
+            plt.scatter(Par_x_plot,Par_y_plot, c=Par_q_plot, cmap=cm.seismic)
+            plt.colorbar(label = '$q$')
             plt.xlim(x_min,x_max)
             plt.ylim(y_min,y_max)
             plt.draw()
@@ -325,7 +412,7 @@ for t in range(0,nt):
         #ax = fig.add_subplot(gs[0,0])
         #ax.contourf(X,Y,E_mag)
         #ax.colorbar(label = '$|E|$')
-        ax.set_title('$t=$ {}'.format(round(t*dt,2)))
+        ax.set_title('$t$ = {}, Particle Number = {}'.format(round(t*dt,2),T_PPC))
         #ax.quiver(X,Y,Par_vx_plot,Par_vy_plot,color='white')
         #ax1 = fig.add_subplot(gs[1,0])
         ax.scatter(Par_x_plot,Par_y_plot, c=Par_q_plot, cmap=cm.seismic)
